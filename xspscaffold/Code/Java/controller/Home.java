e461f39327cc7fa484ce2673b83b6c8e84a13864
Jesse Gallagher <jesse@secondfoundation.org>
2013-01-22 22:15:02 -0500

    Added some controller accoutrements

---
+++ xspscaffold/Code/Java/controller/Home.java
@@ -0,0 +1,8 @@
+package controller;
+
+import frostillicus.controller.BasicXPageController;
+
+public class Home extends BasicXPageController {
+	private static final long serialVersionUID = 1L;
+
+}
