bacf49a0df89af40dd3f97504ed6966e5c7a963a
Jesse Gallagher <jesse@frostillic.us>
2014-04-10 19:23:28 -0400

    Refactored the ModelList classes and added a kludgy fix for "object has been removed or recycled" problem in DominoModelList

--- xspscaffold/Code/Java/frostillicus/model/domino/DominoModelList.java
+++ xspscaffold/Code/Java/frostillicus/model/domino/DominoModelList.java
@@ -23,23 +23,23 @@ public class DominoModelList<E extends AbstractDominoModel> extends AbstractMode
 	private final String filePath_;
 	private final String viewName_;
 	private final String category_;
-	private String sortColumn_;
-	private boolean ascending_;
 	private final List<DominoColumnInfo> columnInfo_;
 	private String searchQuery_;
-	private Class<E> clazz_;
 
 	private final boolean invalid_;
 
 	private Set<Integer> collapsedIds_ = new TreeSet<Integer>();
 	private Set<Integer> expandedIds_ = new TreeSet<Integer>();
 
+	private transient boolean reset_ = false;
+
 	public DominoModelList(final Database database, final String viewName, final String category, final Class<E> clazz) {
+		super(clazz);
+
 		server_ = database.getServer();
 		filePath_ = database.getFilePath();
 		viewName_ = viewName;
 		category_ = category;
-		clazz_ = clazz;
 
 		// Gather the view info now
 		View view = database.getView(viewName_);
@@ -50,7 +50,7 @@ public class DominoModelList<E extends AbstractDominoModel> extends AbstractMode
 
 	protected E createFromViewEntry(final ViewEntry entry, final List<DominoColumnInfo> columnInfo) {
 		try {
-			Constructor<E> con = clazz_.getConstructor(ViewEntry.class, List.class);
+			Constructor<E> con = getClazz().getConstructor(ViewEntry.class, List.class);
 			return con.newInstance(entry, columnInfo);
 		} catch (NoSuchMethodException nsme) {
 			throw new RuntimeException(nsme);
@@ -92,6 +92,13 @@ public class DominoModelList<E extends AbstractDominoModel> extends AbstractMode
 						getCache().put(index, createFromViewEntry(nav.getCurrent(), columnInfo_));
 					} catch(NullPointerException npe) {
 						// Then we've probably hit an "Object has been removed or recycled" on the nav
+
+						if(!reset_) {
+							reset_ = true;
+							clearCache();
+							return get(index);
+						}
+
 						System.out.println("=============================== NPE in DominoModelList");
 						System.out.println("=============================== Current class: " + getClass().getName());
 						System.out.println("=============================== Desired index: " + index);
@@ -115,11 +122,7 @@ public class DominoModelList<E extends AbstractDominoModel> extends AbstractMode
 		try {
 			if (searchQuery_ == null || searchQuery_.isEmpty()) {
 				ViewNavigator nav = getNavigator();
-				int hiddenEntries = 0;
-				/*for (int hiddenCount : collapsedPositions_.values()) {
-					hiddenEntries += hiddenCount;
-				}*/
-				return nav == null ? 0 : getNavigator().getCount() - hiddenEntries;
+				return nav == null ? 0 : getNavigator().getCount();
 			} else {
 				ViewEntryCollection vec = getEntries();
 				return vec == null ? 0 : vec.getCount();
@@ -129,137 +132,16 @@ public class DominoModelList<E extends AbstractDominoModel> extends AbstractMode
 		}
 	}
 
-	public boolean add(final E e) {
-		throw new UnsupportedOperationException();
-	}
-
-	public void add(final int index, final E element) {
-		throw new UnsupportedOperationException();
-	}
-
-	public boolean addAll(final Collection<? extends E> c) {
-		throw new UnsupportedOperationException();
-	}
-
-	public boolean addAll(final int index, final Collection<? extends E> c) {
-		throw new UnsupportedOperationException();
-	}
-
-	public void clear() {
-		throw new UnsupportedOperationException();
-	}
-
-	public boolean contains(final Object o) {
-		throw new UnsupportedOperationException();
-	}
-
-	public boolean containsAll(final Collection<?> c) {
-		for (Object o : c) {
-			if (!contains(o))
-				return false;
-		}
-		return true;
-	}
-
-	public Iterator<E> iterator() {
-		return new DominoListIterator<E>();
-	}
-
-	public int indexOf(final Object o) {
-		for (int i = 0; i < size(); i++) {
-			if (get(i).equals(o)) {
-				return i;
-			}
-		}
-		return -1;
-	}
-
-	public boolean isEmpty() {
-		return size() == 0;
-	}
-
-	public int lastIndexOf(final Object o) {
-		for (int i = size() - 1; i >= 0; i--) {
-			if (get(i).equals(o)) {
-				return i;
-			}
-		}
-		return -1;
-	}
-
-	public ListIterator<E> listIterator() {
-		return new DominoListIterator<E>();
-	}
-
-	public ListIterator<E> listIterator(final int index) {
-		throw new UnsupportedOperationException();
-	}
-
-	public E remove(final int index) {
-		throw new UnsupportedOperationException();
-	}
-
-	public boolean remove(final Object o) {
-		throw new UnsupportedOperationException();
-	}
-
-	public boolean removeAll(final Collection<?> c) {
-		throw new UnsupportedOperationException();
-	}
-
-	public boolean retainAll(final Collection<?> c) {
-		throw new UnsupportedOperationException();
-	}
-
-	public E set(final int index, final E element) {
-		throw new UnsupportedOperationException();
-	}
-
-	public List<E> subList(final int fromIndex, final int toIndex) {
-		throw new UnsupportedOperationException();
-	}
-
-	public Object[] toArray() {
-		Object[] result = new Object[size()];
-		for (int i = 0; i < size(); i++) {
-			result[i] = get(i);
-		}
-		return result;
-	}
-
-	@SuppressWarnings("unchecked")
-	public <T extends Object> T[] toArray(final T[] a) {
-		return (T[]) toArray();
-	};
-
 	/* **********************************************************************
 	 * TabularDataModel methods
 	 ************************************************************************/
 	@Override
-	public int getRowCount() {
-		return size();
-	}
-
-	@Override
-	public Object getRowData() {
-		return get(getRowIndex());
-	}
-
-	@Override
-	public String getResortColumn() {
-		return sortColumn_;
-	}
-
-	@Override
-	public int getResortState(final String columnName) {
+	public boolean isColumnSortable(final String columnName) {
 		if (category_ != null) {
-			return TabularDataModel.RESORT_NONE;
+			return false;
 		}
 
-		if (sortColumn_ != null && sortColumn_.equalsIgnoreCase(columnName)) {
-			return ascending_ ? TabularDataModel.RESORT_ASCENDING : TabularDataModel.RESORT_DESCENDING;
-		}
-		return TabularDataModel.RESORT_NONE;
+		return super.isColumnSortable(columnName);
 	}
 
 	@Override
@@ -284,15 +166,6 @@ public class DominoModelList<E extends AbstractDominoModel> extends AbstractMode
 	}
 
 	@Override
-	public boolean isColumnSortable(final String columnName) {
-		if (category_ != null) {
-			return false;
-		}
-
-		return getResortType(columnName) != TabularDataModel.RESORT_NONE;
-	}
-
-	@Override
 	public void setResortOrder(final String columnName, final String sortOrder) {
 		if(invalid_) return;
 
@@ -307,18 +180,18 @@ public class DominoModelList<E extends AbstractDominoModel> extends AbstractMode
 
 		if (TabularDataModel.SORT_TOGGLE.equals(sortOrder)) {
 			// Cycle between ascending, descending, and off
-			if (ascending_) {
-				ascending_ = false;
-				sortColumn_ = properColumnName;
-			} else if (sortColumn_ == null) {
-				ascending_ = true;
-				sortColumn_ = properColumnName;
+			if (isAscending()) {
+				setAscending(false);
+				setResortColumn(properColumnName);
+			} else if (getResortColumn() == null) {
+				setAscending(true);
+				setResortColumn(properColumnName);
 			} else {
-				sortColumn_ = null;
+				setResortColumn(null);
 			}
 		} else {
-			sortColumn_ = properColumnName;
-			ascending_ = sortOrder.equals(TabularDataModel.SORT_ASCENDING);
+			setResortColumn(properColumnName);
+			setAscending(sortOrder.equals(TabularDataModel.SORT_ASCENDING));
 		}
 	}
 
@@ -326,14 +199,6 @@ public class DominoModelList<E extends AbstractDominoModel> extends AbstractMode
 	public boolean isColumnCategorized(final String columnName) {
 		DominoColumnInfo colInfo = findColumnByName(columnName);
 		return colInfo != null && colInfo.isCategory();
-	}
-
-	@Override
-	public int getRowType() {
-		if (get(getRowIndex()).isCategory()) {
-			return TabularDataModel.TYPE_CATEGORY;
-		}
-		return TabularDataModel.TYPE_ENTRY;
 	}
 
 	@Override
@@ -385,6 +250,7 @@ public class DominoModelList<E extends AbstractDominoModel> extends AbstractMode
 	/* **********************************************************************
 	 * Misc. leftovers
 	 ************************************************************************/
+	@Override
 	public void search(final String searchQuery) {
 		if(invalid_) return;
 
@@ -398,6 +264,7 @@ public class DominoModelList<E extends AbstractDominoModel> extends AbstractMode
 	}
 
 	@SuppressWarnings("deprecation")
+	@Override
 	public E getByKey(final Object key) {
 		View view = getView();
 		ViewEntry entry;
@@ -415,7 +282,7 @@ public class DominoModelList<E extends AbstractDominoModel> extends AbstractMode
 
 	@Override
 	public String toString() {
-		return "[" + getClass().getName() + ": " + server_ + "!!" + filePath_ + "/" + viewName_ + "-" + category_ + sortColumn_ + ascending_ + searchQuery_ + "]";
+		return "[" + getClass().getName() + ": " + server_ + "!!" + filePath_ + "/" + viewName_ + "-" + category_ + getResortColumn() + isAscending() + searchQuery_ + "]";
 	}
 
 	protected ViewNavigator getNavigator() {
@@ -455,8 +322,8 @@ public class DominoModelList<E extends AbstractDominoModel> extends AbstractMode
 		final String key = "viewentries-" + this.toString();
 		if (!requestScope.containsKey(key)) {
 			View view = getView();
-			if (sortColumn_ != null) {
-				view.FTSearchSorted(searchQuery_, 0, sortColumn_, ascending_, false, false, false);
+			if (getResortColumn() != null) {
+				view.FTSearchSorted(searchQuery_, 0, getResortColumn(), isAscending(), false, false, false);
 			} else {
 				view.FTSearch(searchQuery_);
 			}
@@ -472,8 +339,8 @@ public class DominoModelList<E extends AbstractDominoModel> extends AbstractMode
 		view.setAutoUpdate(false);
 		view.setEnableNoteIDsForCategories(true);
 		if (category_ == null) {
-			if (sortColumn_ != null) {
-				view.resortView(sortColumn_, ascending_);
+			if (getResortColumn() != null) {
+				view.resortView(getResortColumn(), isAscending());
 			} else {
 				view.resortView();
 			}
@@ -500,53 +367,5 @@ public class DominoModelList<E extends AbstractDominoModel> extends AbstractMode
 			cacheScope.put(key, new HashMap<Integer, E>());
 		}
 		return (Map<Integer, E>) cacheScope.get(key);
-	}
-
-	private class DominoListIterator<K> implements ListIterator<K> {
-		private int index_ = 0;
-
-		public void add(final K value) {
-			throw new UnsupportedOperationException();
-		}
-
-		public boolean hasNext() {
-			return index_ < size();
-		}
-
-		public boolean hasPrevious() {
-			return index_ > 0;
-		}
-
-		@SuppressWarnings("unchecked")
-		public K next() {
-			if (index_ == size()) {
-				throw new NoSuchElementException();
-			}
-			return (K) get(index_++);
-		}
-
-		public int nextIndex() {
-			return index_ == size() ? index_ : index_ + 1;
-		}
-
-		@SuppressWarnings("unchecked")
-		public K previous() {
-			if (index_ == 0) {
-				throw new NoSuchElementException();
-			}
-			return (K) get(index_--);
-		}
-
-		public int previousIndex() {
-			return index_ == 0 ? index_ : index_ - 1;
-		}
-
-		public void remove() {
-			throw new UnsupportedOperationException();
-		}
-
-		public void set(final K value) {
-			throw new UnsupportedOperationException();
-		}
 	}
 }
