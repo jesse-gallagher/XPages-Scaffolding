f55c556c46b4080c2aeb4b5ba9aef1a85a098edd
Jesse Gallagher <jesse@frostillic.us>
2014-05-28 15:23:06 -0400

    Added @ManagedBean annotation classes

---
+++ frostillicus.framework/src/frostillicus/bean/ManagedBean.java
@@ -0,0 +1,9 @@
+package frostillicus.bean;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(value=RetentionPolicy.RUNTIME)
+public @interface ManagedBean {
+	String name();
+}
---
+++ frostillicus.framework/src/frostillicus/bean/SessionScoped.java
@@ -0,0 +1,7 @@
+package frostillicus.bean;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(value=RetentionPolicy.RUNTIME)
+public @interface SessionScoped { }
