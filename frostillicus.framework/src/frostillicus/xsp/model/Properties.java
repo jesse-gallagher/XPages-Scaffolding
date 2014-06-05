a0af0df493f4026d5f91be47b2b970da1e59bd2d
Jesse Gallagher <jesse@frostillic.us>
2014-06-05 14:16:19 -0400

    - Added an update site - Added a "Properties" annotation for model objects

---
+++ frostillicus.framework/src/frostillicus/xsp/model/Properties.java
@@ -0,0 +1,10 @@
+package frostillicus.xsp.model;
+
+import java.lang.annotation.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface Properties {
+	String[] value();
+	boolean exhaustive() default false;
+}
