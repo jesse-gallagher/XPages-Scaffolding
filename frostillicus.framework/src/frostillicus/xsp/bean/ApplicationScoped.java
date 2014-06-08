fce58e937f794831e5ff26995a121226c5e3327f
Jesse Gallagher <jesse@frostillic.us>
2014-06-08 14:31:57 -0400

    Specified a target type for the bean annotations

--- frostillicus.framework/src/frostillicus/xsp/bean/ApplicationScoped.java
+++ frostillicus.framework/src/frostillicus/xsp/bean/ApplicationScoped.java
@@ -1,7 +1,10 @@
 package frostillicus.xsp.bean;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 @Retention(value=RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
 public @interface ApplicationScoped { }
--- frostillicus.framework/src/frostillicus/xsp/bean/ManagedBean.java
+++ frostillicus.framework/src/frostillicus/xsp/bean/ManagedBean.java
@@ -1,9 +1,12 @@
 package frostillicus.xsp.bean;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 @Retention(value=RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
 public @interface ManagedBean {
 	String name();
 }
