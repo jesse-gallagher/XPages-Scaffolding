d98f7f7fa057aed6e6d4d99300b111c18e27ed34
Paul Withers <paulswithers@hotmail.co.uk>
2014-10-29 23:21:59 +0000

    Fix for opening document using $$OpenDocument - pageName then needs to be "/"+pageClassName Setting isMobile on each page

--- frostillicus.framework/src/frostillicus/xsp/controller/ControllingViewHandler.java
+++ frostillicus.framework/src/frostillicus/xsp/controller/ControllingViewHandler.java
@@ -1,5 +1,6 @@
 package frostillicus.xsp.controller;
 
+import java.util.Map;
 
 import javax.faces.application.ViewHandler;
 import javax.faces.component.UIViewRoot;
@@ -7,14 +8,14 @@ import javax.faces.context.FacesContext;
 import javax.faces.el.MethodBinding;
 import javax.faces.event.PhaseEvent;
 
-import frostillicus.xsp.util.FrameworkUtils;
+import org.openntf.domino.Database;
+import org.openntf.domino.Document;
+import org.openntf.domino.Form;
 
 import com.ibm.commons.util.StringUtil;
 import com.ibm.xsp.component.UIViewRootEx;
 
-import org.openntf.domino.*;
-
-import java.util.*;
+import frostillicus.xsp.util.FrameworkUtils;
 
 public class ControllingViewHandler extends com.ibm.xsp.application.ViewHandlerExImpl {
 	public static final String BEAN_NAME = "controller";
@@ -28,13 +29,15 @@ public class ControllingViewHandler extends com.ibm.xsp.application.ViewHandlerE
 	public UIViewRoot createView(final FacesContext context, final String pageName) {
 		// Page name is in the format "/home"
 		String pageClassName = pageName.substring(1);
+		String checkedPageName = pageName;
 
-		if(pageClassName.equalsIgnoreCase("$$OpenDominoDocument")) {
-			// Handle the virtual document page. There may be a better way to do this, but this will do for now
-			Database database = (Database)resolveVariable(context, "database");
-			Map<String, String> param = (Map<String, String>)resolveVariable(context, "param");
+		if (pageClassName.equalsIgnoreCase("$$OpenDominoDocument")) {
+			// Handle the virtual document page. There may be a better way to do
+			// this, but this will do for now
+			Database database = (Database) resolveVariable(context, "database");
+			Map<String, String> param = (Map<String, String>) resolveVariable(context, "param");
 			Document doc = database.getDocumentByUNID(param.get("documentId"));
-			if(doc != null) {
+			if (doc != null) {
 				String formName = doc.getItemValueString("Form");
 
 				// Now that we have the form, look for the XPageAlt
@@ -44,41 +47,53 @@ public class ControllingViewHandler extends com.ibm.xsp.application.ViewHandlerE
 				Document formDoc = database.getDocumentByUNID(formUNID);
 				String xpageAlt = formDoc.getItemValueString("$XPageAlt");
 
-				if(StringUtil.isEmpty(xpageAlt)) {
+				if (StringUtil.isEmpty(xpageAlt)) {
 					pageClassName = formName;
 				} else {
 					pageClassName = FrameworkUtils.strLeftBack(xpageAlt, ".xsp");
 				}
+
+				// We want to load the class related to the XPage the document
+				// opens with, not a $$OpenDocument XPage
+				checkedPageName = "/" + pageClassName;
 			}
 		}
 
 		Class<? extends XPageController> controllerClass = null;
 		try {
-			controllerClass = (Class<? extends XPageController>)context.getContextClassLoader().loadClass("controller." + pageClassName);
-		} catch(ClassNotFoundException cnfe) {
+			controllerClass = (Class<? extends XPageController>) context.getContextClassLoader().loadClass("controller." + pageClassName);
+		} catch (ClassNotFoundException cnfe) {
 			controllerClass = BasicXPageController.class;
 		}
 		UIViewRootEx root = null;
 		try {
 			XPageController pageController = controllerClass.newInstance();
-			Map<String, Object> requestScope = (Map<String, Object>)resolveVariable(context, "requestScope");
+			pageController.setMobile(FrameworkUtils.isMobile());
+			Map<String, Object> requestScope = (Map<String, Object>) resolveVariable(context, "requestScope");
 			requestScope.put(BEAN_NAME, pageController);
 
-			root = (UIViewRootEx)super.createView(context, pageName);
+			root = (UIViewRootEx) super.createView(context, checkedPageName);
 			root.getViewMap().put(BEAN_NAME, pageController);
 			requestScope.remove(BEAN_NAME);
 
-			MethodBinding beforeRenderResponse = context.getApplication().createMethodBinding("#{" + BEAN_NAME + ".beforeRenderResponse}", new Class[] { PhaseEvent.class });
+			MethodBinding beforeRenderResponse = context.getApplication().createMethodBinding("#{" + BEAN_NAME + ".beforeRenderResponse}",
+					new Class[] { PhaseEvent.class });
 			root.setBeforeRenderResponse(beforeRenderResponse);
 
-			MethodBinding afterRenderResponse = context.getApplication().createMethodBinding("#{" + BEAN_NAME + ".afterRenderResponse}", new Class[] { PhaseEvent.class });
+			MethodBinding afterRenderResponse = context.getApplication().createMethodBinding("#{" + BEAN_NAME + ".afterRenderResponse}",
+					new Class[] { PhaseEvent.class });
 			root.setAfterRenderResponse(afterRenderResponse);
 
-			MethodBinding afterRestoreView = context.getApplication().createMethodBinding("#{" + BEAN_NAME + ".afterRestoreView}", new Class[] { PhaseEvent.class });
+			MethodBinding afterRestoreView = context.getApplication().createMethodBinding("#{" + BEAN_NAME + ".afterRestoreView}",
+					new Class[] { PhaseEvent.class });
 			root.setAfterRestoreView(afterRestoreView);
-		} catch(Exception e) {
+
+			if (StringUtil.isEmpty(root.getPageTitle())) {
+				root.setPageTitle(pageClassName);
+			}
+		} catch (Exception e) {
 			e.printStackTrace();
-			root = (UIViewRootEx)super.createView(context, pageName);
+			root = (UIViewRootEx) super.createView(context, checkedPageName);
 		}
 
 		return root;
