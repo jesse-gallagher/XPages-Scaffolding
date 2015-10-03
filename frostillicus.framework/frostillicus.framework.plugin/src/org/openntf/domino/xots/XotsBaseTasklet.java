02ddfd1da9435dd4be996bcbf0545f7e62bb69db
Jesse Gallagher <jesse@iknowsomeguys.com>
2015-10-03 11:54:43 -0400

    Added an empty XotsBaseTasklet class to get some of my own apps to shut up

---
+++ frostillicus.framework/frostillicus.framework.plugin/src/org/openntf/domino/xots/XotsBaseTasklet.java
@@ -0,0 +1,12 @@
+package org.openntf.domino.xots;
+
+/**
+ * Something, somewhere on my server keeps looking for a class by this name. Add one
+ * here to shut it up.
+ * 
+ * @author jesse
+ *
+ */
+public class XotsBaseTasklet {
+
+}
